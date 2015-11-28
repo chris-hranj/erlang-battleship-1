13f2cdd9c8ce044ddbbefbf2367c383073fd6f57
mjudy94 <judym1@tcnj.edu>
2015-11-28 15:42:49 -0500

    Added preliminary gui files.

---
+++ gui/js/gameplay.js
@@ -0,0 +1,31 @@
+var model = {
+  boardSize: 10;
+  numShips: 5,
+  shipsSunk: 0,
+
+  playerShips: [
+    {locations: [0, 0], hits: ["","",""] },
+    {locations: [0, 0, 0], hits: ["","",""] },
+    {locations: [0, 0, 0], hits: ["","",""] },
+    {locations: [0, 0, 0, 0], hits: ["","","",""] },
+    {locations: [0, 0, 0, 0, 0], hits: ["","","","",""] }
+  ]
+
+  
+};
+
+var view = {
+  
+  updateInstructions: function(instr){
+    
+  }
+  
+  displayHit: function(location){
+    
+  }
+
+  displayMiss: function(location){
+    
+  }
+
+};
