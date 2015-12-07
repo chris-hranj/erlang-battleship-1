c265c74ec74b98359ba037900be0d1ac0c531e3a
Brandon Gottlob <bgottlob1@gmail.com>
2015-12-07 17:46:31 -0500

    Added model file

---
+++ battleship/src/model/game.erl
@@ -0,0 +1,8 @@
+-module(game, [Id,
+                   Player1Board,
+                   Player2Board,
+                   Player1Console,
+                   Player2Console,
+                   Winner,
+                   Turn]).
+-compile(export_all).
