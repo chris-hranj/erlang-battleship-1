1f055b5047faf5d99e5f95471dcf00b0f8572a52
mjudy94 <judym1@tcnj.edu>
2015-12-08 15:08:26 -0500

    Updated Chicago boss routes with player id support

--- battleship/src/controller/battleship_game_controller.erl
+++ battleship/src/controller/battleship_game_controller.erl
@@ -67,20 +67,26 @@ create('POST', []) ->
   NewGame = game:new(id, [], [], [], [], no_one, player1),
   case NewGame:save() of
     {ok, SavedGame} ->
-    {redirect, [{action, "setup"}, {game_id,SavedGame:id()}]};
+    {redirect, [{action, "setup"}, {game_id,SavedGame:id()}, {player, "player1"}]};
     {error, ErrorList} ->
     {ok, [{errors, ErrorList}, {new_msg, NewGame}]}
   end.
 
-join('GET', []) ->
-  ok;
 join('POST', []) ->
-  GameId = Req:post_param("game_id"),
-  ExistingGame = boss_db:find(game, [{game_id,GameId}]).
+  GameId = Req:post_param("id"),
+  {redirect, [{action, "setup"}, {game_id, GameId}, {player, "player2"}]}.
 
-setup('GET', [GameId]) ->
+setup('GET', [GameId, Player]) ->
   Game = boss_db:find(GameId),
-  {ok, [{gameid, Game}]}.
+  {ok, [{gameid, Game}, {player, Player}]};
+setup('POST', []) ->
+  GameId = Req:post_param("game_id"),
+  AircraftPlacement = Req:post_param("carrier"),
+  BattleshipPlacement = Req:post_param("battleship"),
+  DestroyerPlacement = Req:post_param("destroyer"),
+  SubmarinePlacement = Req:post_param("submarine"),
+  PatrolPlacement = Req:post_param("patrol_boat"),
+  ok.
 
 test('GET', []) ->
   ok;
