3918bfe58b8d83129844d77f572a09c9852309bc
chris-hranj <sprtn034@gmail.com>
2015-12-09 14:07:04 -0500

    add live updating list page

---
+++ battleship/priv/init/battleship_news.erl
@@ -0,0 +1,18 @@
+-module(battleship_01_news).
+
+-export([init/0, stop/1]).
+
+% This script is first executed at server startup and should
+% return a list of WatchIDs that should be cancelled in the stop
+% function below (stop is executed if the script is ever reloaded).
+init() ->
+	{ok, WatchId} = boss_news:watch("games", 
+	fun(created, NewGame) -> 
+			boss_mq:push("new-games", NewGame);
+		(deleted, OldGame) ->
+			boss_mq:push("old-games", OldGame)
+	end),
+	{ok, [WatchId]}.
+
+stop(ListOfWatchIDs) ->
+    lists:map(fun boss_news:cancel_watch/1, ListOfWatchIDs).
